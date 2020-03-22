492e1772c942aa064cb3df1260bc82d6742bec96
IshiiKouto <takuminn717@gmail.com>
2020-03-22 15:32:14 +0900

    First Commit

---
+++ Assets/Scenes/ButtonControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonControl : MonoBehaviour
+{
+    public bool isActive = false;
+    public GameObject sunaarashi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sunaarashi.SetActive(isActive);
+    }
+
+    public void buttonOn()
+    {
+        isActive =!isActive;
+        Debug.Log("Button Downed!");
+        Debug.Log(isActive);
+    }
+}
